57bf7c03cac224f28bb01fee9304c01869153b78
Dal ferro Christian <christiandalferro@gmail.com>
2025-09-30 22:12:47 +0200

    ajout message erreur

--- components/DragAndDropUpload.js
+++ components/DragAndDropUpload.js
@@ -1,30 +1,55 @@
 import { Form, Upload, Button, Input } from "antd";
 import { InboxOutlined } from "@ant-design/icons";
+import { useState } from "react";
+import { Spin } from "antd";
 
 const { Dragger } = Upload;
 
+const contentStyle = {
+  position: "absolute",
+  background: "rgba(0, 100, 0, 0.05)",
+  borderRadius: 4,
+  color: "red",
+};
+
+const content = <div style={contentStyle} />;
+
 const DragAndDropUpload = () => {
   const [form] = Form.useForm();
+  const [upload, setUpload] = useState(false);
+  const [messageErreur, setMessageErreur] = useState("rr");
 
   const onFinish = async (values) => {
-    console.log(values)
+    setUpload(true);
     const formData = new FormData();
+    console.log(values , !values.name , !values.files)
+    if (!values.name) {
+      setUpload(false);
+      setMessageErreur("Le champ Nom doit être complété");
+      setTimeout(()=>setMessageErreur(""),1000);
+      return;
+    }
+    if (!values.files) {
+      setUpload(false);
+      setMessageErreur("Aucun fichier sélectionné");
+      setTimeout(()=>setMessageErreur(""), 1000);
+      return;
+    }
     formData.append("name", values.name || "");
-
     // Ajout des fichiers depuis le fileList
     values.files?.forEach((fileWrapper) => {
       formData.append("fichiers", fileWrapper.originFileObj);
     });
-    console.log(formData)
     try {
       const res = await fetch("https://mathsapp-back.vercel.app/users", {
-      //const res = await fetch("http://localhost:3000/users", {
+        //const res = await fetch("http://localhost:3000/users", {
         method: "POST",
         body: formData,
       });
       const data = await res.json();
       console.log("Réponse du back:", data);
       data.result && form.resetFields();
+      setUpload(false);
     } catch (err) {
       console.error("Erreur upload:", err);
     }
@@ -46,7 +71,7 @@ const DragAndDropUpload = () => {
         valuePropName="fileList"
         getValueFromEvent={(e) => (Array.isArray(e) ? e : e?.fileList)}
       >
-        <Dragger multiple beforeUpload={() => false}>
+        <Dragger multiple beforeUpload={() => false} maxCount={3}>
           <p className="ant-upload-drag-icon">
             <InboxOutlined />
           </p>
@@ -60,7 +85,9 @@ const DragAndDropUpload = () => {
         <Button type="primary" htmlType="submit">
           Envoyer
         </Button>
-        <Button htmlType="button" onClick={onReset} className="w-25 mx-10">
+        {upload && <Spin size="large" />}
+        {messageErreur && <p className="text-red-300">{messageErreur}</p>}
+        <Button htmlType="button" onClick={onReset} className="w-25 ">
           Reset
         </Button>
       </div>
