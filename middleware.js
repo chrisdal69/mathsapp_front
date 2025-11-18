42199097988777966588df37ab519e86ae360365
Dal ferro Christian <christiandalferro@gmail.com>
2025-11-18 10:30:20 +0100

    modif middleware.sj

--- middleware.js
+++ middleware.js
@@ -1,20 +1,29 @@
 import { NextResponse } from "next/server";
+
 const NODE_ENV = process.env.NODE_ENV;
 const URL_BACK = process.env.NEXT_PUBLIC_URL_BACK;
 const urlFetch = NODE_ENV === "production" ? URL_BACK : "http://localhost:3000";
 
-
 export async function middleware(req) {
-  console.log("target dans middelware.js : ", req.cookies.get("jwt")?.value);
+  if (!req.nextUrl.pathname.startsWith("/admin")) {
+    return NextResponse.next();
+  }
 
-  if (!req.nextUrl.pathname.startsWith("/admin")) return NextResponse.next();
   const cookie = req.cookies.get("jwt")?.value;
-  if (!cookie) return NextResponse.redirect(new URL("/", req.url));
+  if (!cookie) {
+    return NextResponse.redirect(new URL("/", req.url));
+  }
 
   const resp = await fetch(`${urlFetch}/auth/me`, {
-    headers: { cookie: req.headers.get("cookie") ?? `jwt=${cookie}` },
+    // on renvoie tous les cookies reçus, ou au minimum le jwt
+    headers: {
+      cookie: req.headers.get("cookie") ?? `jwt=${cookie}`,
+    },
   });
-  if (!resp.ok) return NextResponse.redirect(new URL("/", req.url));
+
+  if (!resp.ok) {
+    return NextResponse.redirect(new URL("/", req.url));
+  }
 
   const data = await resp.json();
   if (data.user?.role !== "admin") {
@@ -22,4 +31,5 @@ export async function middleware(req) {
   }
   return NextResponse.next();
 }
+
 export const config = { matcher: ["/admin/:path*"] };
